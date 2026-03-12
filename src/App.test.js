683084c249754db47dd3c443319da0f741e90640
Wajahat <youremail@example.com>
2026-03-13 00:21:04 +0500

    dkk

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Wajahat from './wajahat';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<Wajahat />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
