d589badfd63d532d7123e3d4eb23622a1ecb631d
Wajahat <youremail@example.com>
2026-03-13 00:31:08 +0500

    dkk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from "react";
 //exam
-function App() {
+function Wajahat() {
   const initialTime = 30;
 
   const [isLight, setIsLight] = useState(false);
@@ -140,4 +140,4 @@ function App() {
   );
 }
 
-export default App;
+export default Wajahat;
